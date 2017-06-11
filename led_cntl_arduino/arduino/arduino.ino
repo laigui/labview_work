47469e7f776c7349340f485552ad734ae01c6b59
laigui <laigui@gmail.com>
2017-06-11 17:52:29 +0800

    Update coding style

--- led_cntl_arduino/arduino/arduino.ino
+++ led_cntl_arduino/arduino/arduino.ino
@@ -2,30 +2,28 @@
 * @Author: mikeqin
 * @Date:   2017-06-10 18:36:42
 * @Last Modified by:   mikeqin
-* @Last Modified time: 2017-06-10 18:42:09
+* @Last Modified time: 2017-06-11 17:22:00
 */
 
-byte  comdata;                  // define a variable for storing the rx data
+byte rx_data_byte;              // for storing the rx data
 int LED = 13;                   // define digitial port 13 as LED control pin
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);           // initial baudrate to be 9600
-  pinMode(LED, OUTPUT);         // set LED control pin as output mode
+  pinMode(LED, OUTPUT);         // set LED control pin as output
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available() > 0)   // continously check serial buf availability
-  {
-    comdata = Serial.read();    // read one byte from rx buf to the var.
-    if (comdata == 0x01)        // check if turn off command
-    {
-      digitalWrite(LED, LOW);   // turn off the LED
+  // process only when you receive data
+  if (Serial.available() > 0) {
+    rx_data_byte = Serial.read();     // read one byte from rx buf to the var.
+
+    if (rx_data_byte == 0x01) {
+      digitalWrite(LED, LOW);         // turn off the LED
     }
-    if (comdata == 0x02)        // check if turn on command
-    {
-      digitalWrite(LED, HIGH);  // turn on the LED
+
+    if (rx_data_byte == 0x02) {
+      digitalWrite(LED, HIGH);        // turn on the LED
     }
   }
 }
--- multi_ch_datalogger_arduino/arduino/arduino.ino
+++ multi_ch_datalogger_arduino/arduino/arduino.ino
@@ -2,23 +2,20 @@
 * @Author: mikeqin
 * @Date:   2017-06-10 19:46:09
 * @Last Modified by:   mikeqin
-* @Last Modified time: 2017-06-11 15:44:57
+* @Last Modified time: 2017-06-11 17:23:09
 */
 #define A0_DAQ_CMD  0x10      // A0 channel DAQ command word
 #define A1_DAQ_CMD  0x11      // A1 channel DAQ command word
 #define D0_DAQ_CMD  0x20      // D0 channel DAQ command word
 #define D1_DAQ_CMD  0x21      // D1 channel DAQ command word
 
-
 byte  rx_data_bytes[3] = {0}; // for received serial data frame bytes
 int   ana_val = 0;            // for A/D convert value
 float ana_val_in_volt = 0.0;  // for A/D convert value in uint of voltage
 int   dig_val = 0;            // for digital value
 
-
 void read_data_frame(void);
 void command_handler(void);
-
 
 void setup() {
   Serial.begin(9600);         // opens serial port at 9600 bps
@@ -26,15 +23,13 @@ void setup() {
   pinMode(3, INPUT);          // sets the digital pin 3 as input
 }
 
-
 void loop() {
   // send data only when you receive data
-  while (Serial.available() > 0) {
+  if (Serial.available() > 0) {
     read_data_frame();        // read the received frame bytes in serial buffer
     command_handler();        // handle the command in the received data frame
   }
 }
-
 
 void read_data_frame(void) {
   // read the incoming frame bytes in the serial buffer
@@ -44,7 +39,6 @@ void read_data_frame(void) {
                               // otherwise the data may lost
   }
 }
-
 
 void command_handler(void) {
   // check the header for a valid frame
