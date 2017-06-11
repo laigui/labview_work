0a824fab69aa02ce203a3ec640105a742882074d
laigui <laigui@gmail.com>
2017-06-11 15:53:26 +0800

    Update the coding style

--- multi_ch_datalogger_arduino/arduino/arduino.ino
+++ multi_ch_datalogger_arduino/arduino/arduino.ino
@@ -2,85 +2,77 @@
 * @Author: mikeqin
 * @Date:   2017-06-10 19:46:09
 * @Last Modified by:   mikeqin
-* @Last Modified time: 2017-06-10 22:09:17
+* @Last Modified time: 2017-06-11 15:44:57
 */
-#define A0_CMD  0x10      // A0 channel acq command word
-#define A1_CMD  0x11      // A1 channel acq command word
-#define D0_CMD  0x20      // D0 channel acq command word
-#define D1_CMD  0x21      // D1 channel acq command word
+#define A0_DAQ_CMD  0x10      // A0 channel DAQ command word
+#define A1_DAQ_CMD  0x11      // A1 channel DAQ command word
+#define D0_DAQ_CMD  0x20      // D0 channel DAQ command word
+#define D1_DAQ_CMD  0x21      // D1 channel DAQ command word
 
 
-byte  rxdata[3] = {0};    // for incoming serial data bytes
-int   adVal = 0;          // define variable for A/D convert value
-float fAdVal = 0.0;       // define float var. for A/D convert value
-int   digVal = 0;         // define variable for digital value
+byte  rx_data_bytes[3] = {0}; // for received serial data frame bytes
+int   ana_val = 0;            // for A/D convert value
+float ana_val_in_volt = 0.0;  // for A/D convert value in uint of voltage
+int   dig_val = 0;            // for digital value
 
 
-void receiveData(void);   // receive rx data func.
-void testDoData(void);    // test rx data and do command
+void read_data_frame(void);
+void command_handler(void);
 
 
-void setup() 
-{
-  Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
+void setup() {
+  Serial.begin(9600);         // opens serial port at 9600 bps
+  pinMode(2, INPUT);          // sets the digital pin 2 as input
+  pinMode(3, INPUT);          // sets the digital pin 3 as input
 }
 
 
-void loop() 
-{
-  // send data only when you receive data:
-  while (Serial.available() > 0) 
-  {
-    // read the incoming byte:
-    receiveData();
-    testDoData();
+void loop() {
+  // send data only when you receive data
+  while (Serial.available() > 0) {
+    read_data_frame();        // read the received frame bytes in serial buffer
+    command_handler();        // handle the command in the received data frame
   }
 }
 
 
-void receiveData(void) 
-{
-  int i;
-  for (int i = 0; i < 3; ++i)
-  {
-    rxdata[i] = Serial.read();
-    delay(2);             // delay for uart buf to be ready for next byte
+void read_data_frame(void) {
+  // read the incoming frame bytes in the serial buffer
+  for (int i = 0; i < 3; ++i) {
+    rx_data_bytes[i] = Serial.read();
+    delay(2);                 // delay 2 milliseconds before next reading
+                              // otherwise the data may lost
   }
 }
 
 
-void testDoData(void) 
-{
-  if (rxdata[0] == 0x55)
-  {
-    if (rxdata[1] == 0xAA)
-    {
-      switch (rxdata[2]) 
-      {
-        case A0_CMD:
-          // do something
-          adVal = analogRead(A0);
-          fAdVal = (float)adVal/1023*5.00;
-          Serial.print(fAdVal, 2);
-          break;
-        case A1_CMD:
-          // do something
-          adVal = analogRead(A1);
-          fAdVal = (float)adVal/1023*5.00;
-          Serial.print(fAdVal, 2);
-        case D0_CMD:
-          // do something
-          digVal = digitalRead(2);
-          Serial.print(digVal);
-          break;
-        case D1_CMD:
-          // do something
-          digVal = digitalRead(3);
-          Serial.print(digVal);
-          break;      
-      }
+void command_handler(void) {
+  // check the header for a valid frame
+  if ((rx_data_bytes[0] == 0x55) && (rx_data_bytes[1] == 0xAA)) {
+    // send data according to the command word
+    switch (rx_data_bytes[2]) {
+      case A0_DAQ_CMD:
+        // channel A0 analog signal DAQ
+        ana_val = analogRead(A0);         // read the analog port at pin 0
+        ana_val_in_volt = (float)ana_val/1023*5.00;
+        Serial.print(ana_val_in_volt, 2); 
+        break;
+      case A1_DAQ_CMD:
+        // channel A1 analog signal DAQ
+        ana_val = analogRead(A1);         // read the analog port at pin 1
+        ana_val_in_volt = (float)ana_val/1023*5.00;
+        Serial.print(ana_val_in_volt, 2);
+        break;
+      case D0_DAQ_CMD:
+        // channel D0 digital signal DAQ
+        dig_val = digitalRead(2);         // read the digital port at pin 2
+        Serial.print(dig_val);
+        break;
+      case D1_DAQ_CMD:
+        // channel D1 digital signal DAQ
+        dig_val = digitalRead(3);         // read the digital port at pin 3
+        Serial.print(dig_val);
+        break;      
     }
   }
 }
