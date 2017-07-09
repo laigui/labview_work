b02e7f74eade8271224aa37d67b34fa1d28b05f5
laigui <laigui@gmail.com>
2017-07-09 22:25:32 +0800

    Update coding style.

--- led_cntl_arduino/arduino/arduino.ino
+++ led_cntl_arduino/arduino/arduino.ino
@@ -1,29 +1,60 @@
-/*
-* @Author: mikeqin
-* @Date:   2017-06-10 18:36:42
-* @Last Modified by:   mikeqin
-* @Last Modified time: 2017-06-11 17:22:00
-*/
+/**
+ * @file arduino.ino
+ *
+ * @brief Arduino application
+ *
+ * This program demonstrates how to configure the C8051F410 to write to & read
+ * from the UART interface. The program reads a word using the UART interrupts
+ * and outputs that word to the screen, with all characters in uppercase
+ *
+ * Target:         Arduino Duo
+ * Tool chain:     Arduino IDE 1.8.2
+ * Command Line:   None
+ *
+ * @author         Mike Qin (laigui)
+ * @version        1.0
+ * @date           2017-07-09
+ * @bug No known bugs.
+ *
+ * Revision history:
+ * $Log:$
+ *
+ */
 
-byte rx_data_byte;              // for storing the rx data
-int LED = 13;                   // define digitial port 13 as LED control pin
+byte rx_data_byte;              ///< for storing the rx data
+int LED = 13;                   ///< define digitial port 13 as LED control pin
 
+/**
+ * @brief setup
+ *
+ * put your setup code here, to run once.
+ *
+ * @return void
+ */
 void setup() {
-  Serial.begin(9600);           // initial baudrate to be 9600
-  pinMode(LED, OUTPUT);         // set LED control pin as output
+  Serial.begin(9600);           
+  pinMode(LED, OUTPUT);       
 }
 
+/**
+ * @brief loop
+ * 
+ * put your main code here, to run repeatedly
+ *
+ * @return void
+ */
 void loop() {
   // process only when you receive data
   if (Serial.available() > 0) {
-    rx_data_byte = Serial.read();     // read one byte from rx buf to the var.
+    // read one byte from rx buf to the var
+    rx_data_byte = Serial.read();     
 
     if (rx_data_byte == 0x01) {
-      digitalWrite(LED, LOW);         // turn off the LED
+      digitalWrite(LED, LOW);         
     }
 
     if (rx_data_byte == 0x02) {
-      digitalWrite(LED, HIGH);        // turn on the LED
+      digitalWrite(LED, HIGH);        
     }
   }
 }
--- multi_ch_datalogger_arduino/arduino/arduino.ino
+++ multi_ch_datalogger_arduino/arduino/arduino.ino
@@ -1,45 +1,90 @@
-/*
-* @Author: mikeqin
-* @Date:   2017-06-10 19:46:09
-* @Last Modified by:   mikeqin
-* @Last Modified time: 2017-06-11 17:23:09
-*/
+/**
+ * @file arduino.ino
+ *
+ * @brief Arduino application
+ *
+ * This program demonstrates how to configure the C8051F410 to write to & read
+ * from the UART interface. The program reads a word using the UART interrupts
+ * and outputs that word to the screen, with all characters in uppercase
+ *
+ * Target:         Arduino Duo
+ * Tool chain:     Arduino IDE 1.8.2
+ * Command Line:   None
+ *
+ * @author         Mike Qin (laigui)
+ * @version        1.0
+ * @date           2017-07-09
+ * @bug No known bugs.
+ *
+ * Revision history:
+ * $Log:$
+ *
+ */
+
 #define A0_DAQ_CMD  0x10      // A0 channel DAQ command word
 #define A1_DAQ_CMD  0x11      // A1 channel DAQ command word
 #define D0_DAQ_CMD  0x20      // D0 channel DAQ command word
 #define D1_DAQ_CMD  0x21      // D1 channel DAQ command word
 
-byte  rx_data_bytes[3] = {0}; // for received serial data frame bytes
-int   ana_val = 0;            // for A/D convert value
-float ana_val_in_volt = 0.0;  // for A/D convert value in uint of voltage
-int   dig_val = 0;            // for digital value
+byte  rx_data_bytes[3] = {0}; ///< for received serial data frame bytes
+int   ana_val = 0;            ///< for A/D convert value
+float ana_val_in_volt = 0.0;  ///< for A/D convert value in uint of voltage
+int   dig_val = 0;            ///< for digital value
 
 void read_data_frame(void);
 void command_handler(void);
 
+/**
+ * @brief setup
+ *
+ * put your setup code here, to run once.
+ *
+ * @return void
+ */
 void setup() {
-  Serial.begin(9600);         // opens serial port at 9600 bps
-  pinMode(2, INPUT);          // sets the digital pin 2 as input
-  pinMode(3, INPUT);          // sets the digital pin 3 as input
+  Serial.begin(9600);         
+  pinMode(2, INPUT);          
+  pinMode(3, INPUT);          
 }
 
+/**
+ * @brief loop
+ * 
+ * put your main code here, to run repeatedly
+ *
+ * @return void
+ */
 void loop() {
   // send data only when you receive data
   if (Serial.available() > 0) {
-    read_data_frame();        // read the received frame bytes in serial buffer
-    command_handler();        // handle the command in the received data frame
+    // read the received frame bytes in serial buffer
+    read_data_frame();           
+
+    // handle the command in the received data frame
+    command_handler();
   }
 }
 
+/**
+ * @brief read data frame
+ *
+ * @return void
+ */
 void read_data_frame(void) {
   // read the incoming frame bytes in the serial buffer
   for (int i = 0; i < 3; ++i) {
     rx_data_bytes[i] = Serial.read();
-    delay(2);                 // delay 2 milliseconds before next reading
-                              // otherwise the data may lost
+
+    // delay 2 milliseconds before next reading otherwise the data may lost
+    delay(2);                                               
   }
 }
 
+/**
+ * @brief command handler
+ * 
+ * @return void
+ */
 void command_handler(void) {
   // check the header for a valid frame
   if ((rx_data_bytes[0] == 0x55) && (rx_data_bytes[1] == 0xAA)) {
@@ -47,24 +92,24 @@ void command_handler(void) {
     switch (rx_data_bytes[2]) {
       case A0_DAQ_CMD:
         // channel A0 analog signal DAQ
-        ana_val = analogRead(A0);         // read the analog port at pin 0
+        ana_val = analogRead(A0);         
         ana_val_in_volt = (float)ana_val/1023*5.00;
         Serial.print(ana_val_in_volt, 2); 
         break;
       case A1_DAQ_CMD:
         // channel A1 analog signal DAQ
-        ana_val = analogRead(A1);         // read the analog port at pin 1
+        ana_val = analogRead(A1);         
         ana_val_in_volt = (float)ana_val/1023*5.00;
         Serial.print(ana_val_in_volt, 2);
         break;
       case D0_DAQ_CMD:
         // channel D0 digital signal DAQ
-        dig_val = digitalRead(2);         // read the digital port at pin 2
+        dig_val = digitalRead(2);         
         Serial.print(dig_val);
         break;
       case D1_DAQ_CMD:
         // channel D1 digital signal DAQ
-        dig_val = digitalRead(3);         // read the digital port at pin 3
+        dig_val = digitalRead(3);       
         Serial.print(dig_val);
         break;      
     }
